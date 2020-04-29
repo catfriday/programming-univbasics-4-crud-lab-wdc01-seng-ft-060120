2d52a59ebc14eb6a9d3bdb60ec9a4c466203d6d7
Catrina Friday <cat.friday@gmail.com>
2020-04-29 01:40:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,11 +25,10 @@ end
 def remove_element_from_start_of_array(array)
   array=["wow", "array"]
   array=array.shift 
-  
-  
 end
 
 def retrieve_element_from_index(array, index_number)
+  
   
 end
 
